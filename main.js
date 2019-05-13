3735d0e785e209489b9a3531aa5ab1b1c47dc1b7
liyf <787441351@qq.com>
2019-05-13 14:53:21 +0800

    1.0.0

---
+++ main.js
@@ -0,0 +1,21 @@
+// import Vue from 'vue/dist/vue.esm';
+// import Vuex from 'vuex/dist/vuex.esm';
+// Vue.use(Vuex);
+// const  Counter ={
+//     template: `<div>{{ count }}</div>`,
+//     computed: {
+//         count () {
+//             return this.$store.state.count
+//         }
+//     }
+// };
+// const app = new Vue({
+//     el:'#app',
+//     store,
+//     component:{Counter},
+//     template:`
+//         <div class="app">
+//             <counter></counter>
+//         </div>
+//     `
+// });
