a4912a45144b151ab2cc1a53babde10fa61faf49
liyf <787441351@qq.com>
2019-05-13 15:12:46 +0800

    完成webpack基础配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,35 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     mode: "development",
-    entry: './src/index.js',
+    entry: {
+        app:'./src/index.js',
+        print:'./src/print.js'
+    },
+    devtool: "inline-source-map",
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: "Webpack App Vue"
+        })
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader','css-loader']
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: ['file-loader']
-            }
-        ]
-    }
+
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.css$/,
+    //             use: ['style-loader','css-loader']
+    //         },
+    //         {
+    //             test: /\.(png|svg|jpg|gif)$/,
+    //             use: ['file-loader']
+    //         }
+    //     ]
+    // }
 };
