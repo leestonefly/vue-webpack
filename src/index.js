1aa70aae9658a2b4e92c0b761664a1e07acbde70
liyf <787441351@qq.com>
2019-05-13 15:43:58 +0800

    完成热插拔模块替换功能配置，注意代ç 替换作用域

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import printMe from './print';
-
+import './css/style.css';
 
 function component() {
     var element = document.createElement('div');
@@ -16,3 +16,10 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+if(module.hot){
+    module.hot.accept('./print.js',function () {
+        console.log('Accepting the updated printMe module!');
+        printMe();
+    })
+}
