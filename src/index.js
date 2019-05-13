625d050564ee700940c50b69762bfd302f3cab69
liyf <787441351@qq.com>
2019-05-13 16:24:12 +0800

    完成不同生产环境的web配置

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 import _ from 'lodash';
 import printMe from './print';
 import './css/style.css';
+import {cube} from './math';
 
 function component() {
     var element = document.createElement('div');
+    var Pre = document.createElement('pre');
     var btn = document.createElement('button');
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    Pre.innerText=['Hello webpack!','5 cubed is equal to '+ cube(5)].join('\n\n');
     btn.innerHTML = 'Click me and check the console!';
     btn.onclick = printMe;
 
     element.appendChild(btn);
-
+    element.appendChild(Pre);
     return element;
 }
 
 document.body.appendChild(component());
 
-if(module.hot){
-    module.hot.accept('./print.js',function () {
-        console.log('Accepting the updated printMe module!');
-        printMe();
-    })
-}
+// 热插拔模块定义
+// if(module.hot){
+//     module.hot.accept('./print.js',function () {
+//         console.log('Accepting the updated printMe module!');
+//         printMe();
+//     })
+// }
--- webpack.common.js
+++ webpack.common.js
@@ -1,22 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-// const webpack = require('webpack');
+
 module.exports = {
-    mode: "development",
     entry: {
         app:'./src/index.js',
-
-    },
-    devtool: "inline-source-map",
-    devServer: {
-        contentBase:'./dist',
-        // hot:true
     },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: "Webpack App Vue"
+            title: "Production"
         }),
         // new webpack.NamedModulesPlugin(),
         // new webpack.HotModuleReplacementPlugin()
@@ -24,7 +17,6 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: "/"
     },
     module: {
         rules: [
