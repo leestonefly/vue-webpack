f4b1eb7f537217a4542d8d57eee4de1981a96527
lee <787441351@qq.com>
2019-05-13 21:09:02 +0800

    更正错误

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     configureWebpack: {
